546763acab5d460c44549b34c5b20197733daa9b
dinirugamage0-lab <dinirugamage0@gmail.com>
2026-04-24 14:11:50 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ dns.setServers(['8.8.8.8']);
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-require('dotenv').config();
+
 const app = express();
 app.use(cors());
 app.use(express.json());
